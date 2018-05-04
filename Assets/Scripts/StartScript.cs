65b3ce1b77d7516debb5e63829cf03cca8775534
Hosokawa takuto <>
2018-05-04 09:55:53 +0900

    no message

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour {
+	public GameObject startButton;
+	public int titleflag = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (titleflag == 2) {
+			SceneManager.LoadScene ("Main");
+		}
+	}
+
+	public void StartButton(){
+		titleflag = 2;
+	}
+}
